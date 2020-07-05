3cab9cf2d27e30d1eda6d3538f3fb7da88cb7b74
Duarte Brito <sytertzp@gmail.com>
2020-07-05 14:37:15 +0100

    .

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'atlas-Taming-calculator'`, () => {
+  it(`should have as title 'atlas-taming-calculator'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('atlas-Taming-calculator');
+    expect(app.title).toEqual('atlas-taming-calculator');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to atlas-Taming-calculator!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to atlas-taming-calculator!');
   });
 });
